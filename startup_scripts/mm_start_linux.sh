746dd10c4fc247fd498d0e72f90d6fa101c74fd3
HadiM <marelo64@gmail.com>
2013-10-21 13:42:09 +0200

    Add cmake files

---
+++ startup_scripts/mm_start_linux.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+MM_PATH=$(pwd)
+IJ_LIB=$MM_PATH/lib/micro-manager
+TOOLS_JAR_PATH=$(find /usr -name tools.jar)
+
+export LD_LIBRARY_PATH=.:$IJ_LIB:$LD_LIBRARY_PATH
+export CLASSPATH=$MM_PATH/plugins/Micro-Manager
+
+env java -mx4000m \
+     -Djava.library.path=$IJ_LIB \
+     -Dplugins.dir=$MM_PATH \
+     -cp ij.jar:$TOOLS_JAR_PATH \
+     ij.ImageJ
